2bb64433ba59202ddfedd8018d065f1b89dba22b
Rubio <paulocezarrubio@gmail.com>
2023-04-10 11:39:34 -0300

    req6//test

---
+++ tests/priority_queue/mockqueue.py
@@ -0,0 +1,19 @@
+mock1 = {
+    "nome_do_arquivo": "xdd.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": [
+      "teste test testes"
+      "bonitos testes x"
+      "loren testes"
+  ]
+}
+mock2 = {
+    "nome_do_arquivo": "xd.txt",
+    "qtd_linhas": 4,
+    "linhas_do_arquivo": [
+      "teste test testes"
+      "bonitos testes x"
+      "loren testes"
+      "loren testes"
+  ]
+}
--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,30 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from tests.priority_queue.mockqueue import mock1
+from tests.priority_queue.mockqueue import mock2
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority = PriorityQueue()
+
+    assert priority.__len__() == 0
+    assert priority.is_priority(mock1) is True
+    assert priority.is_priority(mock2) is True
+    assert priority.dequeue() is None
+    assert priority.enqueue(mock1) is None
+    assert priority.enqueue(mock2) is None
+    assert priority.__len__() == 2
+    assert priority.search(0) is mock1
+    assert priority.search(1) is mock2
+    assert priority.dequeue() is mock1
+    assert priority.__len__() == 1
+    assert priority.dequeue() is mock2
+    assert priority.__len__() == 0
+
+    with pytest.raises(IndexError):
+        priority.search(100) is None
+        priority.search(-1) is None
+
+
+
+
