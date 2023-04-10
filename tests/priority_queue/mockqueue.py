eb9995a6f98c5e960287acfac0492b83d82515dd
Rubio <paulocezarrubio@gmail.com>
2023-04-10 11:55:52 -0300

    fix

--- tests/priority_queue/mockqueue.py
+++ tests/priority_queue/mockqueue.py
@@ -2,19 +2,19 @@ mock1 = {
     "nome_do_arquivo": "xdd.txt",
     "qtd_linhas": 3,
     "linhas_do_arquivo": [
-      "teste test testes"
-      "bonitos testes x"
-      "loren testes"
-  ]
+        "teste test testes"
+        "bonitos testes x"
+        "loren testes"
+    ]
 }
 
 mock2 = {
     "nome_do_arquivo": "xd.txt",
     "qtd_linhas": 4,
     "linhas_do_arquivo": [
-      "teste test testes"
-      "bonitos testes x"
-      "loren testes"
-      "loren testes"
-  ]
+        "teste test testes"
+        "bonitos testes x"
+        "loren testes"
+        "loren testes"
+    ]
 }
